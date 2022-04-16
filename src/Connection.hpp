256fde32b62333123825429736c072702f2b843b
yassine-alaoui <alaoui6b@gmail.com>
2022-04-16 11:44:54 +0000

    checking for body data after header

--- src/Connection.hpp
+++ src/Connection.hpp
@@ -21,6 +21,15 @@ namespace we
             Idle,
             Timeout
         };
+
+        // checkers for if body is expected after headers and if it's chucked or not
+        bool                                                                is_body_expected;
+        bool                                                                is_body_chunked;
+
+        // remaining data after end of headers
+        std::string                                                         client_remaining_data;
+
+
         int                                                                 connected_socket;
         int                                                                 client_sock;
         struct sockaddr_storage                                             client_addr;
@@ -54,6 +63,10 @@ namespace we
         void    handle_connection();
 
     private:
+        typedef std::map<std::string, std::string, LessCaseInsensitive>     map_type;
+
+    private:
+        void Connection::check_potential_body(const map_type &);
         bool    is_protocol_supported(const std::string &);
         bool    is_method_supported(const std::string &);
         bool    is_crlf(std::string::const_iterator, std::string::const_iterator);
